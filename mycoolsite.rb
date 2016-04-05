64585bf88c5844906a3068de9c6b1f5e89ad27ed
Valerie <val.conklin@gmail.com>
2016-04-05 13:04:54 -0700

    Static site exists in Sinatra, not using any ERB stuff yet but all is working

---
+++ mycoolsite.rb
@@ -0,0 +1,47 @@
+require 'sinatra'
+
+class MyCoolSite < Sinatra::Base  
+  get '/' do
+    send_file 'static/index.html'
+  end
+
+# why does this require .html when jeremy's didn't?
+
+  get '/about' do
+    send_file 'static/about.html'
+  end
+
+  get '/portfolio' do
+    send_file 'static/portfolio.html'
+  end
+
+  get '/blog' do
+    send_file 'static/blog.html'
+  end
+
+  get '/index.html' do
+    send_file 'static/index.html'
+  end
+
+  get '/about.html' do
+    send_file 'static/about.html'
+  end
+
+  get '/portfolio.html' do
+    send_file 'static/portfolio.html'
+  end
+
+  get '/blog.html' do
+    send_file 'static/blog.html'
+  end
+
+  get '/blog/03-22-2016.html' do
+    send_file 'static/blog/03-22-2016.html'
+  end
+
+  get '/blog/03-25-2016.html' do
+    send_file 'static/blog/03-25-2016.html'
+  end
+
+run!
+end
